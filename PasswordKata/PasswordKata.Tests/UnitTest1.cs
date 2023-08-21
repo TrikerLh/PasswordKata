b7d3271a10a4ad04c7dfde9a91e8ab71d9860e3c
Raúl Corchero <corchero100@gmail.com>
2023-08-21 07:35:47 +0100

    Create proyects

---
+++ PasswordKata/PasswordKata.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace PasswordKata.Tests {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
